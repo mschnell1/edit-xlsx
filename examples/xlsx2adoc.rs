fff593d335c0873f40ee35752464023db41744d6
mschnell <mschnell@lumino.de>
2024-04-22 16:06:40 +0200

    beatify

--- examples/xlsx2adoc.rs
+++ examples/xlsx2adoc.rs
@@ -96,6 +96,64 @@ fn find_col_width(sheet: &WorkSheet) -> Result<Vec<f64>, Error> {
     Ok(widths)
 }
 
+#[derive(Default)]
+struct Cell {
+    text: String,
+    text_color: FormatColor,
+    bg_color: FormatColor,
+    bg_bg_color: FormatColor,
+}
+
+fn get_cell(row: u32, col: u32, sheet: &WorkSheet, widths: &Vec<f64>) -> Cell {
+    let cell_content = sheet.read_cell((row + 1, col + 1)).unwrap_or_default();
+    let format = cell_content.format;
+    let mut cell = Cell::default();
+    if format.is_some() {
+        let format = format.unwrap();
+        cell.text_color = *format.get_color();
+        let ff = format.get_background().clone();
+        cell.bg_color = ff.fg_color;
+        cell.bg_bg_color = ff.bg_color;
+    }
+
+    let cell_text = cell_content.text;
+    cell.text = match cell_text {
+        Some(t) => t,
+        None => "".to_owned(),
+    };
+    cell
+}
+
+fn write_tab_start(writer: &mut BufWriter<&mut File>, widths: &Vec<f64>) -> Result<(), Error> {
+    let bounds = "|===\r";
+    let line = tab_header(&widths);
+    writer.write_all(line.as_bytes())?;
+    writer.write_all(bounds.as_bytes())?;
+    Ok(())
+}
+
+fn write_tab_end(writer: &mut BufWriter<&mut File>) -> Result<(), Error> {
+    let bounds = "|===\r";
+    writer.write_all(bounds.as_bytes())?;
+    Ok(())
+}
+
+fn write_row_delimiter(writer: &mut BufWriter<&mut File>) -> Result<(), Error> {
+    writer.write_all("|".as_bytes())?;
+    Ok(())
+}
+
+
+fn write_col_end(writer: &mut BufWriter<&mut File>) -> Result<(), Error> {
+    writer.write_all("\r".as_bytes())?;
+    Ok(())
+}
+
+fn write_cell(cell: &Cell, writer: &mut BufWriter<&mut File>) -> Result<(), Error> {
+    writer.write_all(cell.text.as_bytes())?;
+    Ok(())
+}
+
 pub fn xlsx_convert(
     in_file_name: &Path,
     out_file_name: &Path,
@@ -106,80 +164,39 @@ pub fn xlsx_convert(
 
     let reading_sheet = workbook.get_worksheet(1);
     let sheet = match reading_sheet {
-      Ok(s) =>  s,
-      Err(e) => return Err(error_text(&format!("{:?}", e))),
+        Ok(s) => s,
+        Err(e) => return Err(error_text(&format!("{:?}", e))),
     };
-
-    let default_row_hight = sheet.get_default_row();
-/* 
-    println!(
-        "Rows {} -> ? ( {} )",
-        sheet.max_row(),
-        default_row_hight
-    );
- */
-    let mut hights = Vec::<f64>::new();
-    for _ in 0..sheet.max_row() {
-        hights.push(default_row_hight);
-    }
-
+    /*
+       let default_row_hight = sheet.get_default_row();
+       let mut hights = Vec::<f64>::new();
+       for _ in 0..sheet.max_row() {
+           hights.push(default_row_hight);
+       }
+    */
     let widths = find_col_width(sheet)?;
-    let bounds = "|===\r";
-    let line = tab_header(&widths);
 
     let mut output_file = File::create(out_file_name)?; // overwrites existing file
     let mut writer = BufWriter::new(&mut output_file);
 
-    writer.write(line.as_bytes())?;
-    writer.write(bounds.as_bytes())?;
+    write_tab_start(&mut writer, &widths)?;
 
     for row in 0..sheet.max_row() {
-/* 
-        println!("Row {} ({})", row, hights[row as usize]);
- */        
+        /*
+               println!("Row {} ({})", row, hights[row as usize]);
+        */
         for col in 0..sheet.max_column() {
             if col < sheet.max_column() {
-                writer.write("|".as_bytes())?;
+                write_row_delimiter(&mut writer)?;
             }
 
-            let cell_content = sheet.read_cell((row + 1, col + 1)).unwrap_or_default();
-            let format = cell_content.format;
-            let mut text_color = FormatColor::Default;
-            let mut bg_color = FormatColor::Default;
-            let mut bg_bg_color = FormatColor::Default;
-            if format.is_some() {
-                let format = format.unwrap();
-                text_color = format.get_color().clone();
-                let ff = format.get_background().clone();
-                bg_color = ff.fg_color.clone();
-                bg_bg_color = ff.bg_color.clone();
-            }
-/* 
-            let cell_format_string = format!(
-                "Text-Color = {:?}        bg = {:?}        bg_bg = {:?}",
-                text_color, bg_color, bg_bg_color
-            );
- */            
-            let cell_text = cell_content.text;
-            let text = match cell_text {
-                Some(t) => t,
-                None => "".to_owned(),
-            };
-/* 
-            println!(
-                "{} ({}) -> {}     Format: {}",
-                col,
-                widths[(col) as usize],
-                text,
-                cell_format_string
-            );
- */            
-            writer.write(text.as_bytes())?;
+            let cell = get_cell(row, col, sheet, &widths);
+            write_cell(&cell, &mut writer)?;
         }
 
-        writer.write("\r".as_bytes())?;
+        write_col_end(&mut writer)?;
     }
-    writer.write(bounds.as_bytes())?;
+    write_tab_end(&mut writer)?;
 
     let xlsx_2_adoc_test_results = Xlsx2AdocTestResults { v1: 0, v2: 0 };
     Ok(xlsx_2_adoc_test_results)
